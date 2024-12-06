9dac5e220832f3057a65603d9d40bf336ceb89eb
lauramoskalenko <moskalenkolaura@gmail.com>
2024-12-06 22:49:16 +0200

    Add files via upload

---
+++ Converter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Converter
+{
+    public decimal DollarToUah { get; private set; }
+    public decimal EuroToUah { get; private set; }
+
+    // Конструктор
+    public Converter(decimal dollarRate, decimal euroRate)
+    {
+        DollarToUah = dollarRate;
+        EuroToUah = euroRate;
+    }
+
+    // Метод для конвертації гривні в долари
+    public decimal ConvertToDollar(decimal amountInUah)
+    {
+        return amountInUah / DollarToUah;
+    }
+
+    // Метод для конвертації гривні в євро
+    public decimal ConvertToEuro(decimal amountInUah)
+    {
+        return amountInUah / EuroToUah;
+    }
+
+    // Метод для конвертації доларів в гривню
+    public decimal ConvertFromDollar(decimal amountInDollar)
+    {
+        return amountInDollar * DollarToUah;
+    }
+
+    // Метод для конвертації євро в гривню
+    public decimal ConvertFromEuro(decimal amountInEuro)
+    {
+        return amountInEuro * EuroToUah;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Введення курсів валют
+        Console.Write("Enter the dollar rate (UAH to 1 Dollar): ");
+        decimal dollarRate = decimal.Parse(Console.ReadLine());
+
+        Console.Write("Enter the euro rate (UAH to 1 Euro): ");
+        decimal euroRate = decimal.Parse(Console.ReadLine());
+
+        // Створення об'єкта Converter
+        Converter converter = new Converter(dollarRate, euroRate);
+
+        Console.WriteLine("\nChoose conversion option:");
+        Console.WriteLine("1. Convert from UAH to Dollar");
+        Console.WriteLine("2. Convert from UAH to Euro");
+        Console.WriteLine("3. Convert from Dollar to UAH");
+        Console.WriteLine("4. Convert from Euro to UAH");
+
+        int option = int.Parse(Console.ReadLine());
+
+        switch (option)
+        {
+            case 1:
+                Console.Write("Enter amount in UAH: ");
+                decimal amountInUahToDollar = decimal.Parse(Console.ReadLine());
+                Console.WriteLine($"{amountInUahToDollar} UAH = {converter.ConvertToDollar(amountInUahToDollar)} Dollars");
+                break;
+            case 2:
+                Console.Write("Enter amount in UAH: ");
+                decimal amountInUahToEuro = decimal.Parse(Console.ReadLine());
+                Console.WriteLine($"{amountInUahToEuro} UAH = {converter.ConvertToEuro(amountInUahToEuro)} Euros");
+                break;
+            case 3:
+                Console.Write("Enter amount in Dollars: ");
+                decimal amountInDollarToUah = decimal.Parse(Console.ReadLine());
+                Console.WriteLine($"{amountInDollarToUah} Dollars = {converter.ConvertFromDollar(amountInDollarToUah)} UAH");
+                break;
+            case 4:
+                Console.Write("Enter amount in Euros: ");
+                decimal amountInEuroToUah = decimal.Parse(Console.ReadLine());
+                Console.WriteLine($"{amountInEuroToUah} Euros = {converter.ConvertFromEuro(amountInEuroToUah)} UAH");
+                break;
+            default:
+                Console.WriteLine("Invalid option!");
+                break;
+        }
+    }
+}
